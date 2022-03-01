53185921e41d3ae1419f22e87c82162b32bb3d70
LFades <luis@vercel.com>
2022-02-28 22:27:38 -0500

    Doesn't work at all

---
+++ packages/next/server/index.ts
@@ -0,0 +1,4 @@
+export { NextRequest } from './web/spec-extension/request'
+export { NextResponse } from './web/spec-extension/response'
+export { NextFetchEvent } from '../server/web/spec-extension/fetch-event'
+export { NextMiddleware } from '../server/web/types'
